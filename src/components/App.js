f624650842352bf276480b9d30712275e600039f
trevor <nicholastrevor05@gmail.com>
2024-08-19 10:38:23 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function NavBar() {
   );
 }
 
-function Home() {
+function Home () {
   return (
     <div id="home">
       <h1>Home</h1>
@@ -16,15 +16,21 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About () {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About />
     </div>
   );
 }
